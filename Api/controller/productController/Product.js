d3016d4dba345a35228fc0fc9247c47d25b30f33
chhabiraniojha <chhabiraniojha@gmail.com>
2023-12-11 23:44:46 +0530

    addproduct controller changes--product data return

--- Api/controller/productController/Product.js
+++ Api/controller/productController/Product.js
@@ -4,8 +4,8 @@ const product=require('../../models/Product');
 
 exports.addProduct=async (req,res,next)=>{
    try {
-      await product.create(req.body);
-      res.status(200).json({success:true,message:"product successfully added"})
+      const addedProduct=await product.create(req.body);
+      res.status(200).json({success:true,message:"product successfully added",addedProduct})
    } catch (error) {
       res.status(500).json({success:false,message:"error"})
    }
