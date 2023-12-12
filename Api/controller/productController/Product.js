ea3d3d5aa7495716fd2a723480c96e9d47e1cb3c
chhabiraniojha <chhabiraniojha@gmail.com>
2023-12-12 21:50:29 +0530

    product.js controller changed

--- Api/controller/productController/Product.js
+++ Api/controller/productController/Product.js
@@ -7,7 +7,7 @@ exports.addProduct=async (req,res,next)=>{
       const addedProduct=await product.create(req.body);
       res.status(200).json({success:true,message:"product successfully added",addedProduct})
    } catch (error) {
-      res.status(500).json({success:false,message:"error"})
+      res.status(500).json({success:false,message:error})
    }
    
 }
