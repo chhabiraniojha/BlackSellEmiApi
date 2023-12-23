90ef1d1696a5778a2f7e6d768af7e0e2a9cd38ff
chhabiraniojha <chhabiraniojha@gmail.com>
2023-12-23 19:43:37 +0530

    admin route sorted

--- Api/routes/productRoutes/Product.js
+++ Api/routes/productRoutes/Product.js
@@ -7,10 +7,10 @@ router.get('/filteredproducts',productController.getFilteredProducts)
 router.get('/products',productController.getProducts)
 
 // admin routes
-router.post("admin/addproduct",productController.addProduct);
-router.get('admin/allproducts',productController.getAllProducts)
-router.get('admin//allproducts/:id',productController.getSingleProduct)
-router.delete('admin/allproducts/:id',productController.deleteSingleProduct)
+router.post("/admin/addproduct",productController.addProduct);
+router.get('/admin/allproducts',productController.getAllProducts)
+router.get('/admin//allproducts/:id',productController.getSingleProduct)
+router.delete('/admin/allproducts/:id',productController.deleteSingleProduct)
 
 
 module.exports=router;
